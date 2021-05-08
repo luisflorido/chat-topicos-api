0b16ed2b02ffe72a4c9935d6a2fe1f2d194ac145
Luis <luiisflorido@gmail.com>
2021-05-07 23:49:19 -0300

    feat: id to user who sent message

--- database/migrations/1620362660836_message_schema.js
+++ database/migrations/1620362660836_message_schema.js
@@ -8,6 +8,7 @@ class MessageSchema extends Schema {
     this.create('messages', (table) => {
       table.increments()
       table.integer('chat_id').unsigned().references('id').inTable('chats')
+      table.integer('user_id').unsigned().references('id').inTable('users')
       table.text('text')
       table.timestamps()
     })
